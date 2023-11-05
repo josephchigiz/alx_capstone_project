acf4ce4784f1f4cc52bb510a64295b4d56f5a39e
Joseph Ochego <josephochego@gmail.com>
2023-11-06 04:31:00 +0530

    update skills links

--- scripts/script.js
+++ scripts/script.js
@@ -39,3 +39,27 @@ function emailValidation(userInput) {
         return false;
     }
 }
+
+
+// *** email-js *** //
+// Using email js to handle form submissions directly to my email
+(function() {
+    // https://dashboard.emailjs.com/admin/account
+    emailjs.init('aI7QUWYpkLZMaCxeG');
+})();
+
+
+window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        // generate a five digit number for the contact_number variable
+        this.contact_number.value = Math.random() * 100000 | 0;
+        // these IDs from the previous steps
+        emailjs.sendForm('contact_service', 'contact_form', this)
+            .then(function() {
+                console.log('SUCCESS!');
+            }, function(error) {
+                console.log('FAILED...', error);
+            });
+    });
+}
